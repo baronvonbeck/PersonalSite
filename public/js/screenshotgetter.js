4f286a1271c8a365f87ca3c20342c5a5b92c2f04
Brian Becker <brbecker@ucdavis.edu>
2018-07-25 00:07:10 -0700

    removed screenshots

--- public/js/screenshotgetter.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function() {
-	console.log("HERE");
-	// const pageres = new Pageres({delay: 4})
-	// 	.src('cryptomarketchains.com', ['1024x768'])
-	// 	.dest("../img/")
-	// 	.run()
-	// 	.then(() => console.log('done'));
-});

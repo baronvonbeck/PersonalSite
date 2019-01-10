9ec4eb2926d8e141d12441449351e2e3df32be50
Brian Becker <brbecker@ucdavis.edu>
2019-01-09 20:35:34 -0800

    setting up codebuild

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ if (cluster.isMaster) {
     var port = process.env.PORT || 3000;
 
     var server = app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
+        console.log('Server running at localhost:' + port + '/');
     });
 }
